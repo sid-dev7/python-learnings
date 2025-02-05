acf6a31f99a217de7048d879b13d1b85bfcdeb64
Siddharth Chobhe <siddharthchobhe@gmail.com>
2025-02-05 08:39:24 +0530

    Day3 changes

--- Day3.py
+++ Day3.py
@@ -101,7 +101,7 @@
 
 #Code 8 
 
-def function_3(num1, num2, num3):
- print(f"num1 = {num1}, num2 = {num2}, num3 = {num3}")
+# def function_3(num1, num2, num3):
+#  print(f"num1 = {num1}, num2 = {num2}, num3 = {num3}")
 
-function_3(10, 30, 40)
+# function_3(10, 30, 40)
