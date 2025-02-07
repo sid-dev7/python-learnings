b7db9a4323244e81c14b4845851e3e49f413d949
Siddharth Chobhe <siddharthchobhe@gmail.com>
2025-02-07 20:39:11 +0530

    refactoring

--- Day 1 hello world.py
+++
@@ -1,3 +0,0 @@
-print("hello world")
-
-print("day 1 depedency")
--- hello world.py
+++
@@ -1,6 +0,0 @@
-print ("hello world")
-# Ask for the user's name
-name = input("What is your name? ")
-
-# Print a greeting
-print(f"Hello, {name}!")
