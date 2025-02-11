e0a9757920859a8d244d1bc8e3599d52394f670f
Siddharth Chobhe <siddharthchobhe@gmail.com>
2025-02-11 09:53:52 +0530

    day_5 changes

--- day_5/day_5.py
+++ day_5/day_5.py
@@ -1,5 +1,27 @@
-def function1():
-    #emty list
-    list_1 = []
-    print(list_1)
-function1()
+#code:1
+
+# def function1():
+#     #emty list
+#     list_1 = []
+#     print(list_1)
+#     print(f"list type : {type(list_1)}")
+#     #emty list
+#     list_2 = list()
+#     print(list_2)
+#     print(f"list type : {type(list_2)}")
+    
+# function1()
+
+#Code:2
+def function2():
+    list_1 = [10, 20, 30]
+    # iterate 
+
+    for value in list_1:
+        print(value)
+
+# Calling the function
+function2()
+
+
+
