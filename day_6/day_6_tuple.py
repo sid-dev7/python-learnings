497cad372ae810591e67f0d78ea4f92098f22ead
Siddharth Chobhe <siddharthchobhe@gmail.com>
2025-02-12 16:53:39 +0530

    DAY6CHANGE

--- day_6/day_6_tuple.py
+++ day_6/day_6_tuple.py
@@ -1,11 +1,27 @@
-def function1():
-    #list
-    numbers_list = [10, 20, 30, 40, 50]
-    print(f"numbers = {numbers_list}, type = {type(numbers_list)}")
+# def function1():
+#     #list
+#     numbers_list = [10, 20, 30, 40, 50]
+#     print(f"numbers = {numbers_list}, type = {type(numbers_list)}")
 
-    #tuple
-    numbers_tuple = (10, 20, 30, 40, 50, 60)
+#     #tuple
+#     numbers_tuple = (10, 20, 30, 40, 50, 60)
 
-    print(f"numbers_tuple = {numbers_tuple}, type ={type(numbers_tuple)}")
+#     print(f"numbers_tuple = {numbers_tuple}, type ={type(numbers_tuple)}")
 
-function1()
+# function1()
+
+# empty tuple
+tuple_1 = tuple()
+print(tuple_1)
+# integer variable
+tuple_1 = (10)
+print(tuple_1)
+# tuple with one value
+tuple_2 = (10,)
+print(tuple_2)
+# string variable
+tuple_3 = ("test")
+print(tuple_3)
+# tuple with one value
+tuple_4 = ("test",)
+print(tuple_4)
