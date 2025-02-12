a2a7cf958b47ee4575f91ef8591cede4ce380b18
Siddharth Chobhe <siddharthchobhe@gmail.com>
2025-02-12 10:50:57 +0530

    push change

---
+++ day_5/day_5_indexing.py
@@ -0,0 +1,6 @@
+#indexing 
+numbers = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+# numbers[0] = 10
+print(f"numbers[0] = {numbers[0]}")
+# numbers[9] = 100
+print(f"numbers[9] = {numbers[9]}")
--- day_5/day_5_operations.py
+++ day_5/day_5_operations.py
@@ -49,12 +49,6 @@
 
 # function_2()
 
-
-
-
-
-# function_2()
-
 #count operator
 
 # numbers = [10, 20, 30, 40, 60, 70, 80, 40, 90, 100, 40, 110,
@@ -62,16 +56,43 @@
 # # 40 is repeated 4 times 
 # print(f"40 is repeated {numbers.count(40)} times")
 
+# index operator 
 
-numbers = [10, 20, 30, 40, 60, 70, 80, 40, 90, 100, 40, 110,
-40]
-# index of 40 = 3
-print(f"index of 40 = {numbers.index(40, 0)}")
-# index of 40 = 7
-print(f"index of 40 = {numbers.index(40, 4)}")
-# index of 40 = 10
-print(f"index of 40 = {numbers.index(40, 8)}")
-# index of 40 = 12
-print(f"index of 40 = {numbers.index(40, 11)}")
+# numbers = [10, 20, 30, 40, 60, 70, 80, 40, 90, 100, 40, 110,
+# 40]
+# # index of 40 = 3
+# print(f"index of 40 = {numbers.index(40, 0)}")
+# # index of 40 = 7
+# print(f"index of 40 = {numbers.index(40, 4)}")
+# # index of 40 = 10
+# print(f"index of 40 = {numbers.index(40, 8)}")
+# # index of 40 = 12
+# print(f"index of 40 = {numbers.index(40, 11)}")
 
+# sort operator
+# numbers = [10, 2, 3, 1, 8, 3, 4, 6, 5]
+# Ascending
+# [1, 2, 3, 3, 4, 5, 6, 8, 10]
+# numbers.sort()
+# print(numbers)
+# Descending
+# [10, 8, 6, 5, 4, 3, 3, 2, 1]
+# numbers.reverse()
+# print(numbers)
+# numbers.sort(reverse=True)
+# print(numbers)
 
+# clear operator 
+
+# numbers = [10, 30, 50, 40, 20]
+# numbers.clear()
+# # []
+# print(numbers)
+
+# extend operator
+
+# numbers_1 = [10, 20, 30, 40, 50]
+# numbers_2 = [60, 70, 80, 90, 100]
+# numbers_1.extend(numbers_2)
+# # [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+# print(numbers_1)
