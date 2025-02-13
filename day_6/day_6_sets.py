d6ad84dba8cfae99ba826fba85b2c4df47982152
Siddharth Chobhe <siddharthchobhe@gmail.com>
2025-02-13 20:55:12 +0530

    change

---
+++ day_6/day_6_sets.py
@@ -0,0 +1,24 @@
+#inetersections
+s1 = {10, 20, 30, 40}
+s2 = {30, 40, 50, 60}
+# {30, 40}
+print(s1.intersection(s2))
+# {30, 40}
+print(s2.intersection(s1))
+
+#union 
+s1 = {10, 20, 30, 40}
+s2 = {30, 40, 50, 60}
+# {10, 20, 30, 40, 50, 60}
+print(s1.union(s2))
+# {10, 20, 30, 40, 50, 60}
+print(s2.union(s1))
+
+
+#difference 
+s1 = {10, 20, 30, 40}
+s2 = {30, 40, 50, 60}
+# {10, 20}
+print(s1.difference(s2))
+# {50, 60}
+print(s2.difference(s1))
