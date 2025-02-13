87292ac2027127b0ec6de7ca391f68878961b461
Siddharth Chobhe <siddharthchobhe@gmail.com>
2025-02-13 17:08:54 +0530

    changes

--- day_6/day_6_tuple.py
+++ day_6/day_6_tuple.py
@@ -11,17 +11,43 @@
 # function1()
 
 # empty tuple
-tuple_1 = tuple()
-print(tuple_1)
-# integer variable
-tuple_1 = (10)
-print(tuple_1)
-# tuple with one value
-tuple_2 = (10,)
-print(tuple_2)
-# string variable
-tuple_3 = ("test")
-print(tuple_3)
-# tuple with one value
-tuple_4 = ("test",)
-print(tuple_4)
+# tuple_1 = tuple()
+# print(tuple_1)
+# # integer variable
+# tuple_1 = (10)
+# print(tuple_1)
+# # tuple with one value
+# tuple_2 = (10,)
+# print(tuple_2)
+# # string variable
+# tuple_3 = ("test")
+# print(tuple_3)
+# # tuple with one value
+# tuple_4 = ("test",)
+# print(tuple_4)
+
+# def math_operations(p1, p2):
+#     addtion = p1, + p2
+#     substration = p1 - p2
+#     division = p1 / p2
+#     multiplication = p1 * p2
+
+#     return addtion, substration, multiplication, division
+
+# result = math_operations(10, 40)
+
+# print(result)
+
+# list, tuple, list
+
+# numbers_1 = [10, 20, 30, 40, 50]
+
+# print(f"{numbers_1}, type {type(numbers_1)}")
+
+
+# numbers_2 = (10, 20, 30, 40, 50)
+# print(f"{numbers_2}, type {type(numbers_2)}")
+
+# numbers_3 = {10, 20, 30, 40, 50}
+# print(f"{numbers_3}, type {type(numbers_3)}")
+
