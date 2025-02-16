ade4f7d2ad2d6c1e3120a1f944767e85c05e4e20
Siddharth Chobhe <siddharthchobhe@gmail.com>
2025-02-16 22:17:07 +0530

    frozen set

--- day_6/day_6_disctionary.py
+++ day_6/day_6_disctionary.py
@@ -1,8 +1,15 @@
-def function():
-    s1 = {10, 20, 30, 40, 50}
-    s1.add(60)
-    s1.add(40)
-    s1.remove(30)
+# def function():
+#     s1 = {10, 20, 30, 40, 50}
+#     s1.add(60)
+#     s1.add(40)
+#     s1.remove(30)
 
-    print(s1)
-function()
+#     print(s1)
+# function()
+
+# fronzen set imputable (Only prints unique values)
+
+# s1 = frozenset([10, 20, 30, 40, 50, 40, 40])
+
+# print (f" s1= {s1}, type {type(s1)}")
+
