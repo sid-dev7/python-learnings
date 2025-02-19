08cdf024fdae29d2fc6d057281c039272423aaa2
Siddharth Chobhe <siddharthchobhe@gmail.com>
2025-02-19 22:42:57 +0530

    push

--- day_7/day_7_k.py
+++ day_7/day_7_k.py
@@ -1,15 +1,29 @@
-def function1():
+# def function1():
     # single dimentional
-    list1 = [10, 20, 30, 40, 50]
-    print(f"{list1}")
- # list of list 
- #multi dimentional 
-    list2 = [
+    # list1 = [10, 20, 30, 40, 50]
+#     print(f"{list1}")
+#  # list of list 
+#  #multi dimentional 
+#     # list2 = [
+#     #     [10, 20],
+#     #     [30, 40],
+#     #     [50, 60]
+#     ]
+#     print(f"list2[0], {list2[0]}")
+#     print(f"list2[0], {list2[0][0]}")
+#     print(f"list2[0], {list2[0][1]}")
+# function1()
+
+def function2():
+    # list of lists
+    list1 = [
         [10, 20],
-        [30, 40],
-        [50, 60]
+        [30, 40, 50]
+        [60, 70, 80, 90, 100]
     ]
-    print(f"list2[0], {list2[0]}")
-    print(f"list2[0], {list2[0][0]}")
-    print(f"list2[0], {list2[0][1]}")
-function1()
+
+    for row in list1:
+        for col in row:
+            print(f"value = {col}")
+    print(f"-" * 10)
+function2()
