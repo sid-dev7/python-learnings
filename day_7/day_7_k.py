9216de79a184b4857e6bf65d182140106b54f1c0
Siddharth Chobhe <siddharthchobhe@gmail.com>
2025-02-21 10:41:27 +0530

    day7

--- day_7/day_7_k.py
+++ day_7/day_7_k.py
@@ -14,19 +14,19 @@
 #     print(f"list2[0], {list2[0][1]}")
 # function1()
 
-def function2():
-    # list of lists
-    list1 = [
-        [10, 20],
-        [30, 40, 50]
-        [60, 70, 80, 90, 100]
-    ]
 
-    for row in list1:
-        for col in row:
-            print(f"value = {col}")
-    print(f"-" * 10)
-function2()    
+def function3():
+    # List of dictionaries
+    cars = [
+        {"model": "i20", "company": "hyundai", "price": 7.5},
+        {"model": "i10", "company": "hyundai", "price": 10},
+        {"model": "Nano", "company": "TATA", "price": 40},
+    ]  
+    for car in cars:
+        print(f"Model: {car['model']}")
+        print(f"Company: {car['company']}")
+        print(f"Price: {car['price']}")
+        print('-' * 20)
 
-
-
+# Call the function
+function3()
--- git_commands.py
+++ git_commands.py
@@ -1,3 +1,9 @@
-#git commit (file_name)
-#git -m (chnage_name)
-#git push
+# git commit (file_name)
+# git -m (chnage_name)
+# git push
+
+
+# ls (get list)
+# cd (change directory/folder)
+# cd ../ (once step back from current folder)
+# pwd (present working directory)
