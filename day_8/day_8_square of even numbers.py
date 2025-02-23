8f3bfe3303a17c5033dd378f5a337480e7ab3fff
Siddharth Chobhe <siddharthchobhe@gmail.com>
2025-02-23 11:01:53 +0530

    day8push

---
+++ day_8/day_8_square of even numbers.py
@@ -0,0 +1,10 @@
+def function1():
+    numbers = [1,2,3,4,5,6,7,8,9,10]
+
+    even_numbers = list(filter(lambda x : x % 2 ==0, numbers))
+
+    sqaure_even = list(map(lambda x: x ** 2, even_numbers))
+
+    print(sqaure_even)
+
+function1()
