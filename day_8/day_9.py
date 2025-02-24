553ba5eceac80d4b4ea6e1621f4aae13777de149
Siddharth Chobhe <siddharthchobhe@gmail.com>
2025-02-24 17:09:30 +0530

    d9

---
+++ day_8/day_9.py
@@ -0,0 +1,9 @@
+# scope: global
+num = 100
+print(f"outside of any function num = {num}")
+def function1():
+    print("inside function1")
+    print(f"num = {num}")
+    first_name = "steve"
+    print(f"first name = {first_name}")
+function1()
