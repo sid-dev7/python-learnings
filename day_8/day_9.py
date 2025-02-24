d57065816901269b0987f116aacc1d39c8722e08
Siddharth Chobhe <siddharthchobhe@gmail.com>
2025-02-24 17:19:53 +0530

    d9scope

--- day_8/day_9.py
+++ day_8/day_9.py
@@ -4,6 +4,8 @@ print(f"outside of any function num = {num}")
 def function1():
     print("inside function1")
     print(f"num = {num}")
+    
+    # scope: local
     first_name = "steve"
     print(f"first name = {first_name}")
 function1()
---
+++ day_8/day_9_scope.py
@@ -0,0 +1,12 @@
+# scope: global
+num = 100
+print("befor calling function1 num = {num}")
+
+def function1():
+    print(f"inside function1")
+    num = 500
+    print(f"num = {num}")
+function1()
+
+# num = 500
+print(f"after calling function1 num = {num}")
