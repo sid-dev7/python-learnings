f9fd5723efe6321cc7b5b64b779bbd11fcf04ff2
Siddharth Chobhe <siddharthchobhe@gmail.com>
2025-02-27 09:47:19 +0530

    d9

--- day_9/day_9_alloperators.py
+++ day_9/day_9_alloperators.py
@@ -3,20 +3,24 @@ def add(p1, p2):
 
 
 def subtract(p1,p2):
-    print(f"subtraction = {p1, p2}")
+    print(f"subtraction = {p1-p2}")
 
-def multiplication(p1, p2):
-    print(f"subtraction = {p1, p2}")
-
+def multiplication(p1,p2):
+    print(f" mutliplicaiton = {p1*p2}")
 def division(p1, p2):
-    print(f"subtraction = {p1, p2}")
+    print(f"division= {p1/p2}")
 
 def execute(function):
     # function = add
     print(f"function = {function}")
     function(10,20)
-execute(add)
+    function(20,20)
+    function(100,40)
 
+execute(add)
+execute(subtract)
+execute(multiplication)
+execute(division)
 
 
 
