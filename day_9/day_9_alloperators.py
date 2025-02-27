4f9fd207d600aba18ecff666430920b10d041ace
Siddharth Chobhe <siddharthchobhe@gmail.com>
2025-02-27 09:42:24 +0530

    day9

---
+++ day_9/day_9_alloperators.py
@@ -0,0 +1,24 @@
+def add(p1, p2):
+    print(f"addition = {p1+p2}")
+
+
+def subtract(p1,p2):
+    print(f"subtraction = {p1, p2}")
+
+def multiplication(p1, p2):
+    print(f"subtraction = {p1, p2}")
+
+def division(p1, p2):
+    print(f"subtraction = {p1, p2}")
+
+def execute(function):
+    # function = add
+    print(f"function = {function}")
+    function(10,20)
+execute(add)
+
+
+
+
+# (10,20)
+# (20,40)
