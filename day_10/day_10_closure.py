72e8ba2cf0c464843432569af826e732c8227bf8
Siddharth Chobhe <siddharthchobhe@gmail.com>
2025-03-08 10:29:08 +0530

    closure2

--- day_10/day_10_closure.py
+++ day_10/day_10_closure.py
@@ -14,3 +14,13 @@ function('header line 1')
 div = outer('div')
 div('division 1')
 div('division 2')
+
+#closure code 2
+
+def outer():
+
+    def inner():
+        print(f"print inner")
+    return inner
+result = outer()
+print(f" result = {result}, type = {type(result)}")
