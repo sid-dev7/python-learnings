8926026ec7e01b9be0ce5cef1c0486b2a24e3add
Siddharth Chobhe <siddharthchobhe@gmail.com>
2025-03-11 21:04:29 +0530

    d11

--- day_11/day_11.py
+++ day_11/day_11.py
@@ -5,8 +5,12 @@ def add(p1, p2):
 add(10, 20)
 
 def divide(p1, p2):
+    print("😀"*50)
+    print("inside the function divide")
+    print(f"p1 = {p1}, p2 ={p2}")
     division = p1 / p2
     print(f"division = {division}")
+    print("😀"*50)
 divide (20,2)
 
 
