9f61a1d7473b8d79ff2322b4ca3030a046e9472e
Siddharth Chobhe <siddharthchobhe@gmail.com>
2025-03-11 17:33:54 +0530

    decorator

--- day_10/day_10_decorator.py
+++ day_10/day_10_decorator.py
@@ -20,3 +20,20 @@ def  multiply(p1, p2):
 add(10, 20)
 subtract(10, 20)
 multiply(10, 40)
+
+#code:2
+def decorate_result(function):
+    def inner(p1, p2):
+        print("*" * 40)
+        function(p1, p2)
+        print("*" * 40)
+    return inner
+@decorate_result
+def  add(p1, p2):
+    print(f"add = {p1 + p2}")
+@decorate_result
+def subtraction(p1, p2):
+    print(f"subtraction = {p1 - p2}")
+
+add(50, 30)
+subtraction(50,50)
