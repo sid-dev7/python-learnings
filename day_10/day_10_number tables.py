0a85996927e2cd5346fa82d4801d9882df08d202
Siddharth Chobhe <siddharthchobhe@gmail.com>
2025-03-11 16:56:17 +0530

    push

--- day_10/day_10_number tables.py
+++ day_10/day_10_number tables.py
@@ -1,7 +1,9 @@
 def create_table(number):
     def inner (count = 10):
-        table = [number * index for index in range (1, count+1)]
-        print(table)
+        # table = [number * index for index in range (1, count+1)]
+        # print(table)
+        for index in range(1, count+1):
+            print(f"{number} * {index} = {number * index}")
     return inner
 number_2 = create_table(2)
-number_2()
+number_2(20)
