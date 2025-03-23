4e745d7286077975e5e659ad3e8ec72180a38659
Siddharth Chobhe <siddharthchobhe@gmail.com>
2025-03-23 10:34:00 +0530

    day12

---
+++ day_12/day_12_3.py
@@ -0,0 +1,15 @@
+class person:
+    pass
+p1 = person()
+setattr(p1, "name", "address")
+setattr(p1, "age", 30)
+
+print(f"name: {getattr(p1, 'name')}")
+print(f"age: {getattr(p1, 'age')}")
+
+p2 = person()
+setattr(p2, "name", "person2")
+setattr(p2, "age", 10)
+
+print(f"name: {getattr(p2, 'name')}")
+print(f"age: {getattr(p2, 'age')}")
