15a59a8a80fc9a8b2577fcad0f3cc7e21f3a9aab
Siddharth Chobhe <siddharthchobhe@gmail.com>
2025-03-23 10:03:09 +0530

    day12

---
+++ day_12/day_12_class.py
@@ -0,0 +1,12 @@
+#empty class
+class person:
+    pass
+#instantiation: creating an object of class
+# reference = object 
+# object is collection of key value pairs
+person = person()
+print(person)
+print(person.__dict__)
+
+# add new key
+
