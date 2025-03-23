c4ca40f445f21ff2dd1c343322fc9d5c7a538fe0
Siddharth Chobhe <siddharthchobhe@gmail.com>
2025-03-23 10:13:54 +0530

    tetattr,getatter

--- day_12/day_12_class.py
+++ day_12/day_12_class.py
@@ -10,3 +10,15 @@ print(person.__dict__)
 
 # add new key
 
+setattr(person, "name", "person1")
+setattr(person, "address", "pune")
+setattr(person,"age", 27)
+
+# print(person.__dict__)
+
+#get value of attribute name
+
+print(f"name = {getattr(person, 'name')}")
+print(f"address = {getattr(person,'address')}")
+print(f"age = {getattr(person,'age')}")
+
