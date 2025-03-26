46a7d0ee37252055bb759d5e9d787efc25a3db60
Siddharth Chobhe <siddharthchobhe@gmail.com>
2025-03-26 15:17:30 +0530

    oop

--- day_12/day_12.4.py
+++ day_12/day_12.4.py
@@ -1,14 +1,25 @@
 class car:
-    pass
-
-def print_info(car):
-    print(f"model = {car.model}")
-    print(F"Company = {car.company}")
+    # method
+    def print_info(self):
+        print(f"model = {self.model}")
+        print(F"Company = {self.company}")
 c1 = car()
+
+print(car.__dict__)
 
 #adding attributes
 c1.model = "i20"
 c1.company =  "Hyundai"
-c1.print_info = print_info
 
-c1.print_info(c1)
+c1.print_info()
+c2 = car()
+c2.model = "Nexon"
+c2.company = "TATA"
+#car.print_info gets caled iwth c2 value
+c2.print_info()
+# c1.print_info = print_info
+
+# c1.print_info(c1)
+
+
+
