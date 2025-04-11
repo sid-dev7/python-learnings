caa6f595b6b33de078da9e6f5a3e293e4c7447fd
Siddharth Chobhe <siddharthchobhe@gmail.com>
2025-04-11 10:24:31 +0530

    excepct block

--- day_17/day_17_1.py
+++ day_17/day_17_1.py
@@ -2,8 +2,9 @@ def divide (n1,n2):
     #Try block
     try:
         # exception can be generated
-        division =n1/n2
+        division = n1/n2
         print(f"division = {division}")
+    #except block
     except:
         print(f"exception has raised")
 
