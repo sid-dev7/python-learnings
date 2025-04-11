4a621f1f86010a91d9125d40dd03a322e62c6638
Siddharth Chobhe <siddharthchobhe@gmail.com>
2025-04-11 10:23:13 +0530

    try

--- day_17/day_17_1.py
+++ day_17/day_17_1.py
@@ -1,6 +1,10 @@
 def divide (n1,n2):
-    division =n1/n2
+    #Try block
+    try:
+        # exception can be generated
+        division =n1/n2
+        print(f"division = {division}")
+    except:
+        print(f"exception has raised")
 
-    print(f"division = {division}")
-
-divide(10,2)
+divide(10,0)
