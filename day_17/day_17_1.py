e4633cd818dfc05f1c12729f02fc97aa4caf65b0
Siddharth Chobhe <siddharthchobhe@gmail.com>
2025-04-11 10:31:46 +0530

    filenotfound

--- day_17/day_17_1.py
+++ day_17/day_17_1.py
@@ -1,11 +1,21 @@
-def divide (n1,n2):
-    #Try block
-    try:
-        # exception can be generated
-        division = n1/n2
-        print(f"division = {division}")
-    #except block
-    except:
-        print(f"exception has raised")
+# def divide (n1,n2):
+#     #Try block
+#     try:
+#         # exception can be generated
+#         division = n1/n2
+#         print(f"division = {division}")
+#     #except block
+#     except:
+#         print(f"exception has raised")
 
-divide(10,0)
+# divide(10,9)
+
+def read_file():
+    try:
+        file = open("./myfile.txt", "r")
+        data = file.read()
+        print(f"data = {data}")
+        file.close
+    except:
+        print(f"The file does not exist")
+read_file()
