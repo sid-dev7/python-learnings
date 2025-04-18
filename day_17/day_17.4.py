4f1ae168b2aacb3b4f545a909f7be7bc862b649f
Siddharth Chobhe <siddharthchobhe@gmail.com>
2025-04-18 14:47:58 +0530

    d18

--- day_17/day_17.4.py
+++ day_17/day_17.4.py
@@ -9,7 +9,11 @@ def get_user_input():
 
     print("enter age: ")
     age = input()
-
+    #convert the string to int
+    age = int(age)
+    if (age < 0) or (age > 100):
+        #raise an exception
+        raise IndentationError()
     print(f"name = {name}")
     print(f"age = {age}")
-get_user_input
+get_user_input()
---
+++ day_18/day_18_1.py
@@ -0,0 +1,10 @@
+class Person:
+    def __init__(self, name, age):
+        self.__name = name
+        self.__age = age
+    def print_info(self):
+        print(f"name = {self.__name}")
+        print((f"age  = {self.__age}"))
+Person = Person('person 1', 10)
+
+Person.print_info()
