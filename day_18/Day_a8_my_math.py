e699ef7cb9f6a1036335bd2e7e70e79d50ffbe9c
Siddharth Chobhe <siddharthchobhe@gmail.com>
2025-04-20 16:46:58 +0530

    modulesandpackages

---
+++ day_18/Day_a8_my_math.py
@@ -0,0 +1,4 @@
+from my_math import addition, subtraction
+
+print(addition.add(10, 20))        # Output: 30
+print(subtraction.subtract(10, 5)) # Output: 5
---
+++ day_18/day_18_main.py
@@ -0,0 +1,4 @@
+import day_18_calculator
+
+print(day_18_calculator.add(5, 3))         # Output: 8
+print(day_18_calculator.subtract(10, 4))   # Output: 6
