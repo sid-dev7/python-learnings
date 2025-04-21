07dc9ddba93558138e4181b266c3ccfeffb84341
Siddharth Chobhe <siddharthchobhe@gmail.com>
2025-04-21 11:02:17 +0530

    push19

---
+++ day_19/day_19_1.py
@@ -0,0 +1,16 @@
+def function1():
+    # list mutable 
+    numbers = [10, 20, 30 ,40, 50]
+    countries = ["India", "USA", "UK"]
+    
+    numbers.append(60)
+    countries.append("Japan")
+
+    for number in numbers:
+        print(number)
+    
+    print("-"*50)
+
+    for country in countries:
+        print(country)
+function1()
