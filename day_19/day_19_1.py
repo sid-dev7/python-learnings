dba64a398c080ceb3d567df64f7cd9369b9cf4ee
Siddharth Chobhe <siddharthchobhe@gmail.com>
2025-04-21 11:05:24 +0530

    19push

--- day_19/day_19_1.py
+++ day_19/day_19_1.py
@@ -14,3 +14,17 @@ def function1():
     for country in countries:
         print(country)
 function1()
+
+def function2():
+    # tuple (imputable)
+
+    numbers = (10, 20, 30, 40, 50)
+    countries = ("India", "USA", "UK")
+
+    for number in numbers
+        print(number)
+    print("-"*50)
+
+    for country in countries:
+        print(country)
+function2()
