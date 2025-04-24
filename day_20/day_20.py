44b0321c4d9c307f15816c2bd0edad68972d8a3f
Siddharth Chobhe <siddharthchobhe@gmail.com>
2025-04-24 10:48:53 +0530

    mysql

---
+++ day_20/day_20.py
@@ -0,0 +1,6 @@
+# create database personsdb;
+# use personsdb;
+# create table person (id integer primary key auto_increment, name varchar(100), age integer, email varchar(50));
+# insert into person (name, address, age, email) values ('person1', 'Pune', 10, 'person1@test.com');
+# insert into person (name, address, age, email) values ('person2', 'Mumbai', 20, 'person2@test.com');
+# select name from person;
