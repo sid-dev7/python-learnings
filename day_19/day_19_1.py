4f47209cb481a66bdb6f51f406eb98ba9366dc1f
Siddharth Chobhe <siddharthchobhe@gmail.com>
2025-05-03 09:22:11 +0530

    D19

--- day_19/day_19_1.py
+++ day_19/day_19_1.py
@@ -1,30 +1,40 @@
-def function1():
-    # list mutable 
-    numbers = [10, 20, 30 ,40, 50]
-    countries = ["India", "USA", "UK"]
+# def function1():
+#     # list mutable 
+#     numbers = [10, 20, 30 ,40, 50]
+#     countries = ["India", "USA", "UK"]
     
-    numbers.append(60)
-    countries.append("Japan")
+#     numbers.append(60)
+#     countries.append("Japan")
 
-    for number in numbers:
-        print(number)
+#     for number in numbers:
+#         print(number)
     
-    print("-"*50)
+#     print("-"*50)
 
-    for country in countries:
-        print(country)
-function1()
+#     for country in countries:
+#         print(country)
+# function1()
 
-def function2():
-    # tuple (imputable)
+# def function2():
+#     # tuple (imputable)
 
-    numbers = (10, 20, 30, 40, 50)
-    countries = ("India", "USA", "UK")
+#     numbers = (10, 20, 30, 40, 50)
+#     countries = ("India", "USA", "UK")
 
-    for number in numbers
-        print(number)
-    print("-"*50)
+#     for number in numbers
+#         print(number)
+#     print("-"*50)
 
-    for country in countries:
-        print(country)
-function2()
+#     for country in countries:
+#         print(country)
+# function2()
+
+def function3():
+    numbers1 = [10, 20, 30, 40]
+    numbers2 = [50, 60, 70, 80]
+
+    numbers3 = numbers1 + numbers2  
+    print(numbers1)
+    print(numbers2)
+    print(numbers3)
+function3()
--- day_3/Day3.py
+++ day_3/Day3.py
@@ -101,6 +101,6 @@
 
 # Code 8 
 
-def function_3(num1, num2, num3):
- print(f"num1 = {num1}, num2 = {num2}, num3 = {num3}")
-function_3(10, 30, 40)
+# def function_3(num1, num2, num3):
+#  print(f"num1 = {num1}, num2 = {num2}, num3 = {num3}")
+# function_3(10, 30, 40)
