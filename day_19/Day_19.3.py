d50e7759ec62b808682f98e10aeb9344b6c75c27
Siddharth Chobhe <siddharthchobhe@gmail.com>
2025-05-03 10:25:39 +0530

    broadcast operatoers

---
+++ day_19/Day_19.3.py
@@ -0,0 +1,16 @@
+import numpy as np
+def function2():
+    a1 = np.array([1, 2, 3,4])
+    #broadcast mathematical operaters
+    print(a1 +10)
+    print(a1 -10)
+    print(a1 /10)
+    print(a1 *10)
+    #broadcast omparison operators
+    print (a1>2)
+    print (a1>=2)
+    print (a1<2)
+    print (a1<=2)
+    print (a1==2)
+    print (a1!=2)
+function2()
