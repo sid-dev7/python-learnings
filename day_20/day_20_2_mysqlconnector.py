f3be6320087f2f02af78f5eacbb31e9d8d7a5181
Siddharth Chobhe <siddharthchobhe@gmail.com>
2025-05-09 10:56:05 +0530

    operations

--- day_20/day_20_2_mysqlconnector.py
+++ day_20/day_20_2_mysqlconnector.py
@@ -1,2 +1,32 @@
 # command to for mysql connector
 # pip install mysql-connector-python
+
+
+# frist sql to python 
+
+import mysql.connector
+def select_records():
+    # connect to the databse
+    connection = mysql.connector.connect(host='localhost', user='root', password= 'admin123', database= 'personsdb')
+    # create a query
+    query = "select id, name, address, age from perosn"
+
+    # get the cursur(in-memory table)
+    cursor = connection.cursor()
+
+    #  execute the query
+    cursor.execute(query)
+
+    #  get query result
+    result = cursor.fetchall()
+
+    #  process the result
+    print(result)
+
+    #close the cursor 
+
+    cursor.close()
+
+    #close connection
+    connection.close()
+select_records()
