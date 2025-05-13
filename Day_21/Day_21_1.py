7eb55e270b69dd7de0c51f1c43b7c251f04f7976
Siddharth Chobhe <siddharthchobhe@gmail.com>
2025-05-13 10:04:21 +0530

    day21.2

--- Day_21/Day_21_1.py
+++ Day_21/Day_21_1.py
@@ -6,3 +6,12 @@ def function1():
     s1 = pd.Series([10,20,30,40,50])
     print(f" s1= {s1}, type = {type(s1)}")
 function1()
+
+# filtering
+def function2():
+    # one dementional array
+    s1 = pd.Series([10,20,30,40,50])
+    # +ve indexing
+    print(f"value at 0 = {s1[0]}")
+    print(f"value at 4 = {s1[4]}")
+function2()
