5aa171dc3e51a9bd7f5641256b2cdcfcf03aedec
Siddharth Chobhe <siddharthchobhe@gmail.com>
2025-05-13 10:00:26 +0530

    d21

---
+++ Day_21/Day_21_1.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+
+def function1():
+    # one dementional array
+    s1 = pd.Series([10,20,30,40,50])
+    print(f" s1= {s1}, type = {type(s1)}")
+function1()
