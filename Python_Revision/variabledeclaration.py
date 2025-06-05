7102e8cbad8d75bd8cd6dca1a0c4f8ca61ea0d2c
Siddharth Chobhe <siddharthchobhe@gmail.com>
2025-06-05 08:15:33 +0530

    multiline string

--- Python_Revision/variabledeclaration.py
+++ Python_Revision/variabledeclaration.py
@@ -1,6 +1,22 @@
 # declare variable
-num =100
+num = 500
 # print(num)
 # print(type(num))
+
 # use offormated string
-print(f"num = {num}")
+
+# Int
+print(f"num = {num}, datatype = {type(num)}")
+
+salary = 10.78
+# float
+print(f"salary = {salary}, datatype = {type(salary)}")
+
+# string
+Name = "Sid"
+print(f" Name = {Name}, datatype ={type(Name)}")
+
+# multiline string
+address = """birla tower,flat no:5,
+Mumbai, 4100556"""
+print(f"address = {address}, datatype = {type(address)}")
