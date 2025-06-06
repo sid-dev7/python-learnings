6b690d3f4d099cc1427a72a24d84efad8a501bbb
Siddharth Chobhe <siddharthchobhe@gmail.com>
2025-06-06 10:12:06 +0530

    parameterized_function

--- Python_Revision/function.py
+++ Python_Revision/function.py
@@ -5,3 +5,9 @@ def function1():
     print(f"this is function body")
 # function call
 function1()
+
+# parameterized function
+def function2(param):
+    print("inside function2")
+    print(f"param = {param}, type = {type(param)}")
+function2("hello")
