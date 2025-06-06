4bbcc01354cfe1b7850a85ae65f6951b29d76c64
Siddharth Chobhe <siddharthchobhe@gmail.com>
2025-06-06 09:52:18 +0530

    logical operator

---
+++ Python_Revision/logical_operators.py
@@ -0,0 +1,13 @@
+# logical operators
+
+# and => &&
+age =10
+#  age >20 and age < 60
+
+if age > 20:
+    print(f"inside if block")
+    print(f"age is greater than 20")
+else:
+    print(f"inside if block")
+    print("age is less than 60")
+# or => //
