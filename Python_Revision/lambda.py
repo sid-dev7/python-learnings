ba704d3c1173124d1708915dfd26eb0e43aa7edc
Siddharth Chobhe <siddharthchobhe@gmail.com>
2025-06-08 17:08:50 +0530

    mutliply_lambda

--- Python_Revision/lambda.py
+++ Python_Revision/lambda.py
@@ -17,3 +17,10 @@ print(f" addition of 30 + 40 = {addition}")
 lambda_add = lambda p1,p2: p1 + p2
 
 print(f"addtion of 10, 20= {lambda_add(10,20)}")
+
+# lambda for mutliply two numbers
+# accepts two values and returns mutliplication of them
+mutliply = lambda x,y: x*y
+print(f"multiplication = {mutliply(10,4)}")
+
+
