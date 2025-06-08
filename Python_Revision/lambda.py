4160f55714c8ad43faa7c5a52585e7820030e01b
Siddharth Chobhe <siddharthchobhe@gmail.com>
2025-06-08 17:06:28 +0530

    lambda_add

--- Python_Revision/lambda.py
+++ Python_Revision/lambda.py
@@ -7,3 +7,13 @@ cube = lambda number: number **3
 print(f"cube of 9 = {cube(9)}")
 print(f"cube = {cube}")
 print(f"type cube = {type(cube)}")
+
+def add(p1,p2):
+    return p1+p2 
+# myAdd = add
+addition = add(30,40)
+print(f" addition of 30 + 40 = {addition}")
+
+lambda_add = lambda p1,p2: p1 + p2
+
+print(f"addtion of 10, 20= {lambda_add(10,20)}")
