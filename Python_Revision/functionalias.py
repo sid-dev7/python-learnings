d540c5aaa184a3ca2ac2b4b18999b59db43aa48f
Siddharth Chobhe <siddharthchobhe@gmail.com>
2025-06-08 16:58:22 +0530

    lambda

---
+++ Python_Revision/functionalias.py
@@ -0,0 +1,10 @@
+n1= 200
+n2 = 100
+def function1():
+        print (f"inside function1")
+        add = n1 + n2
+        myfunction =function1
+        # print(f'addition = {add}')
+
+        print(f"{myfunction}")
+function1()
---
+++ Python_Revision/lambda.py
@@ -0,0 +1,9 @@
+def square(number):
+    return number**2
+# inline function (lambda function)
+print(f"square of 9 = {square(9)}")
+# labmda function
+cube = lambda number: number **3
+print(f"cube of 9 = {cube(9)}")
+print(f"cube = {cube}")
+print(f"type cube = {type(cube)}")
