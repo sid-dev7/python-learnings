0bfa0f2f9d1b66b5fb895cc612fe664a737ee55a
Siddharth Chobhe <siddharthchobhe@gmail.com>
2025-06-11 14:52:08 +0530

    tupletype

--- Python_Revision/tuple.operations.py
+++ Python_Revision/tuple.operations.py
@@ -1,2 +1,14 @@
+# imuateble cant modify after defyning 
 numbers_tuple = (10,20,30,40,50)
 print(f"numbers = {numbers_tuple}, type={type(numbers_tuple)}")
+
+print("-*-"*20)
+# emty tuple
+
+tuple_1 = ()
+
+print(f"emty tuple = {tuple_1}, type = {type(tuple_1)}")
+# if you do not put commma after any value it would not consider as tuple 
+tuple_2 = (10,)
+
+print(f"emty tuple = {tuple_2}, type = {type(tuple_2)}")
