4749c140738ee1ef90d49fe0101ba27b23b94d3c
Siddharth Chobhe <siddharthchobhe@gmail.com>
2025-06-11 15:08:56 +0530

    set_ops

---
+++ Python_Revision/set_operations.py
@@ -0,0 +1,11 @@
+s1 = {10, 20, 30,40,50}
+s2 = {60,70,80,90,100}
+print(f"s1 union s2 = {s1.union(s2)}")
+print(f"s2 uniion s1 = {s2.union(s1)}")
+
+
+print(f"s1 intersection s2 = {s1.intersection(s2)}")
+print(f"s2 intersection s1 = {s2.intersection(s1)}")
+
+print(f"s1 difference s2 = {s1.difference(s2)}")
+print(f"s2 difference s1 = {s2.difference(s1)}")
