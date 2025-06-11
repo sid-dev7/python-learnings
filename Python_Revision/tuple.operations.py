916d68f0b8e138decaed7ffaf6a7464cde870aac
Siddharth Chobhe <siddharthchobhe@gmail.com>
2025-06-11 15:00:46 +0530

    math ops

--- Python_Revision/tuple.operations.py
+++ Python_Revision/tuple.operations.py
@@ -12,3 +12,13 @@ print(f"emty tuple = {tuple_1}, type = {type(tuple_1)}")
 tuple_2 = (10,)
 
 print(f"emty tuple = {tuple_2}, type = {type(tuple_2)}")
+
+def math_operations(p1,p2):
+
+    add = p1 +p2
+    sub = p1-p2
+    multi = p1*p2
+    div= p1/p2
+    return add, sub,multi,div
+result = math_operations(10,2)
+print(result)
