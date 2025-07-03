7d515f36809cb21ef33f08c7a7f3781b1e1eb896
Siddharth Chobhe <siddharthchobhe@gmail.com>
2025-07-03 09:09:30 +0530

    loging

---
+++ Python_Revision/logging.py
@@ -0,0 +1,18 @@
+def add(p1,p2):
+    print("-"*40)
+    print(f"inside the fucntion add")
+    print(f"p1 ={p1}, p2={p2}")
+    add = p1+p2
+    print(f'addition= {add}')
+    print("-"*40)
+add(10,2)
+
+# divide 
+def divide(p1,p2):
+    print("-"*40)
+    print(f"inside the fucntion divide")
+    print(f"p1 ={p1}, p2={p2}")
+    division = p1/p2
+    print(f' division= {division}')
+    print("-"*40)
+divide(10,0)
